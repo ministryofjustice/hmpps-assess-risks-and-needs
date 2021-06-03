0c20bcdc603ad6485b35750d240ec45183863d18
Stuart Whitlam <stuart.whitlam@digital.justice.gov.uk>
2021-06-03 14:37:52 +0100

    Use correct method to check auth scope values Use correct version of hmpps-auth docker image

--- src/main/kotlin/uk/gov/justice/digital/hmpps/assessrisksandneeds/api/model/SupplementaryRiskDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/assessrisksandneeds/api/model/SupplementaryRiskDto.kt
@@ -7,7 +7,7 @@ import java.util.UUID
 data class SupplementaryRiskDto(
 
   @Schema(description = "Supplementary Risk ID", example = "78beac68-884c-4784-9bea-fd8088f52a47")
-  val supplementaryRiskId: UUID = UUID.randomUUID(),
+  val supplementaryRiskId: UUID? = null,
 
   @Schema(description = "Source of Risk", example = "INTERVENTION_REFERRAL")
   val source: Source,
@@ -28,7 +28,7 @@ data class SupplementaryRiskDto(
   val createdDate: LocalDateTime?,
 
   @Schema(description = "Risk Summary Comments", example = "Free text up to 4000 characters")
-  val riskComments: String,
+  val riskSummaryComments: String?,
 )
 
 enum class Source {
