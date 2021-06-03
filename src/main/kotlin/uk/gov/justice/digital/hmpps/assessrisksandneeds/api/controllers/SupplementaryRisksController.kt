592900f3a0a0c4fe709a12f9c5f6bbb6c281f3b6
Stuart Whitlam <stuart.whitlam@digital.justice.gov.uk>
2021-06-03 09:07:40 +0100

    Add get by source, get by UUID and get by CRN endpoints

--- src/main/kotlin/uk/gov/justice/digital/hmpps/assessrisksandneeds/api/controllers/SupplementaryRisksController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/assessrisksandneeds/api/controllers/SupplementaryRisksController.kt
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.assessrisksandneeds.api.model.Source
 import uk.gov.justice.digital.hmpps.assessrisksandneeds.api.model.SupplementaryRiskDto
+import java.util.*
 
 @RestController
 class SupplementaryRisksController {
@@ -24,11 +25,43 @@ class SupplementaryRisksController {
       ApiResponse(responseCode = "200", description = "OK")
     ]
   )
-  fun getSupplementaryRisk(
+  fun getSupplementaryRiskBySource(
     @Parameter(description = "Source ID", required = true, example = "78beac68-884c-4784-9bea-fd8088f52a47")
     @PathVariable sourceId: String,
-    @Parameter(description = "Source Type", required = true, example = "INTERVENTIONS_REFERRAL")
+    @Parameter(description = "Source Type", required = true, example = "INTERVENTION_REFERRAL")
     @PathVariable sourceType: Source
+  ) {
+    TODO()
+  }
+
+  @RequestMapping(path = ["/risks/supplementary/crn/{crn}"], method = [RequestMethod.GET])
+  @Operation(description = "Gets supplementary risk for a given CRN")
+  @ApiResponses(
+    value = [
+      ApiResponse(responseCode = "403", description = "Unauthorized"),
+      ApiResponse(responseCode = "404", description = "Supplementary risk source Not Found"),
+      ApiResponse(responseCode = "200", description = "OK")
+    ]
+  )
+  fun getSupplementaryRisksByCrn(
+    @Parameter(description = "CRN", required = true, example = "X123456")
+    @PathVariable crn: String
+  ) {
+    TODO()
+  }
+
+  @RequestMapping(path = ["/risks/supplementary/{supplementaryRiskId}"], method = [RequestMethod.GET])
+  @Operation(description = "Gets supplementary risk for a given ID")
+  @ApiResponses(
+    value = [
+      ApiResponse(responseCode = "403", description = "Unauthorized"),
+      ApiResponse(responseCode = "404", description = "Supplementary risk source Not Found"),
+      ApiResponse(responseCode = "200", description = "OK")
+    ]
+  )
+  fun getSupplementaryRiskById(
+    @Parameter(description = "Supplementary ID", required = true, example = "78beac68-884c-4784-9bea-fd8088f52a47")
+    @PathVariable supplementaryRiskId: UUID
   ) {
     TODO()
   }
