c0617da1dd2040dd706b2f27874c8b3f5a0aae31
adelaidaa <adelaida.aj@gmail.com>
2021-07-16 15:14:45 +0200

    upgrade dependencies to fix vulnerabilities (#63)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.4"
   kotlin("plugin.spring") version "1.5.10"
   kotlin("plugin.jpa") version "1.5.10"
 }
@@ -17,13 +17,13 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.security:spring-security-oauth2-client")
   implementation("org.apache.commons:commons-lang3")
-  implementation("org.springdoc:springdoc-openapi-ui:1.5.8")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.8")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.8")
-  implementation("com.zaxxer:HikariCP:4.0.3")
+  implementation("org.springdoc:springdoc-openapi-ui:1.5.9")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.9")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.9")
+  implementation("com.zaxxer:HikariCP:5.0.0")
   implementation("org.postgresql:postgresql")
   runtimeOnly("com.h2database:h2:1.4.200")
-  runtimeOnly("org.flywaydb:flyway-core:7.8.1")
+  runtimeOnly("org.flywaydb:flyway-core:7.11.2")
   testRuntimeOnly("com.h2database:h2:1.4.200")
   testAnnotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
