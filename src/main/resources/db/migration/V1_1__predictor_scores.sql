62fc782165aa799f3e63226cc9c3d01a12b71c40
adelaidaa <adelaida.aj@gmail.com>
2021-08-18 17:41:44 +0100

    add table for predictors

--- src/main/resources/db/migration/V1_1__predictor_scores.sql
+++ src/main/resources/db/migration/V1_1__predictor_scores.sql
@@ -1,10 +1,10 @@
-CREATE TABLE IF NOT EXISTS offender_predictor
+CREATE TABLE IF NOT EXISTS offender_predictors_history
 (
     offender_predictor_id           serial          primary key,
-    offender_predictor_uuid         uuid            not null unique,
+    offender_predictor_uuid         UUID            not null unique,
     predictor_type                  varchar(100)    not null,
     algorithm_version               varchar(10)     not null,
-    predictor_score                 varchar(10)     not null,
+    calculated_at                   timestamp       not null,
     crn                             varchar(20)     not null,
     predictor_trigger_source        varchar(200)    not null,
     source_id                       varchar(200)    not null,
@@ -14,5 +14,17 @@ CREATE TABLE IF NOT EXISTS offender_predictor
     CONSTRAINT unique_source UNIQUE(risk_source, source_id)
 );
 
-CREATE INDEX idx_crn ON offender_predictor(crn);
-CREATE INDEX idx_uuid ON offender_predictor(offender_predictor_uuid);
+CREATE INDEX idx_crn ON offender_predictors_history (crn);
+CREATE INDEX idx_uuid ON offender_predictors_history (offender_predictor_uuid);
+
+CREATE TABLE IF NOT EXISTS predictors
+(
+    predictor_id                  serial  primary key,
+    predictor_uuid                UUID    not null unique,
+    offender_predictor_uuid       UUID        NOT NULL,
+    predictor_subtype             varchar(100)    not null,
+    predictor_score               varchar(10)     not null,
+    predictor_level               varchar(10)     not null,
+    created_date                  timestamp       not null,
+    FOREIGN KEY (offender_predictor_uuid) REFERENCES offender_predictors_history (offender_predictor_uuid)
+);
