b95107fc7335cae9f9d3cb089d153803c4d3bc5f
adelaidaa <adelaida.aj@gmail.com>
2021-08-31 17:53:11 +0200

    use latest dependencies (#81)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.4"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.6"
   kotlin("plugin.spring") version "1.5.10"
   kotlin("plugin.jpa") version "1.5.10"
 }
@@ -17,15 +17,15 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.security:spring-security-oauth2-client")
   implementation("org.apache.commons:commons-lang3")
-  implementation("org.springdoc:springdoc-openapi-ui:1.5.9")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.9")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.9")
+  implementation("org.springdoc:springdoc-openapi-ui:1.5.10")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.10")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.10")
   implementation("com.zaxxer:HikariCP:5.0.0")
   implementation("org.postgresql:postgresql")
   implementation("com.vladmihalcea:hibernate-types-52:2.12.1")
   implementation("com.beust:klaxon:5.5")
   runtimeOnly("com.h2database:h2:1.4.200")
-  runtimeOnly("org.flywaydb:flyway-core:7.11.2")
+  runtimeOnly("org.flywaydb:flyway-core:7.14.0")
   testRuntimeOnly("com.h2database:h2:1.4.200")
   testAnnotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
