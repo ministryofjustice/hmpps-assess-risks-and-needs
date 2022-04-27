6ecc495c08f74d00c0c2829cecc54b5b03b4a30d
Lucas Cairns <lucas.cairns@gmail.com>
2022-04-27 12:01:02 +0100

    Bump dependency versions (#147)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.0"
-  kotlin("plugin.spring") version "1.6.10"
-  kotlin("plugin.jpa") version "1.6.10"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.5"
+  kotlin("plugin.spring") version "1.6.21"
+  kotlin("plugin.jpa") version "1.6.21"
 }
 
 configurations {
@@ -21,16 +21,16 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.security:spring-security-oauth2-client")
   implementation("org.apache.commons:commons-lang3")
-  implementation("org.springdoc:springdoc-openapi-ui:1.6.6")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.6")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.6")
-  implementation("com.zaxxer:HikariCP:5.0.0")
-  implementation("org.postgresql:postgresql:42.3.3")
-  implementation("com.vladmihalcea:hibernate-types-52:2.12.1")
-  implementation("com.beust:klaxon:5.5")
-  implementation("com.microsoft.onnxruntime:onnxruntime:1.10.0")
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.7")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.7")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.7")
+  implementation("com.zaxxer:HikariCP:5.0.1")
+  implementation("org.postgresql:postgresql:42.3.4")
+  implementation("com.vladmihalcea:hibernate-types-52:2.16.1")
+  implementation("com.beust:klaxon:5.6")
+  implementation("com.microsoft.onnxruntime:onnxruntime:1.11.0")
   runtimeOnly("com.h2database:h2:1.4.200")
-  runtimeOnly("org.flywaydb:flyway-core:7.15.0")
+  runtimeOnly("org.flywaydb:flyway-core:8.5.9")
   testRuntimeOnly("com.h2database:h2:1.4.200")
   testAnnotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
@@ -38,6 +38,9 @@ dependencies {
     exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     exclude(module = "mockito-core")
   }
-  testImplementation("com.ninja-squad:springmockk:3.0.1")
+  testImplementation("com.ninja-squad:springmockk:3.1.1")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
+}
+repositories {
+  mavenCentral()
 }
