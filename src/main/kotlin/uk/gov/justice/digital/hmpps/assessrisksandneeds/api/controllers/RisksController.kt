c7499638296ab586601def271e493f275943bf40
mark.chaplin <mark.heyward-chaplin@digital.justice.gov.uk>
2022-07-04 14:54:26 +0100

    ARN-1055 Add swagger api response annotations for user access

--- src/main/kotlin/uk/gov/justice/digital/hmpps/assessrisksandneeds/api/controllers/RisksController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/assessrisksandneeds/api/controllers/RisksController.kt
@@ -101,8 +101,11 @@ class RisksController(
   @Operation(description = "Gets Risk Management Plan from latest complete assessments for crn")
   @ApiResponses(
     value = [
-      ApiResponse(responseCode = "403", description = "Unauthorized"),
-      ApiResponse(responseCode = "404", description = "CRN Not Found"),
+      ApiResponse(responseCode = "403", description = "User does not have permission to access offender with provided CRN"),
+      ApiResponse(responseCode = "404", description = "Risk management plan data does not exist for CRN"),
+      ApiResponse(responseCode = "404", description = "Offender does not exist in Delius for provided CRN"),
+      ApiResponse(responseCode = "404", description = "User does not exist in Delius for provided user name"),
+      ApiResponse(responseCode = "401", description = "Unauthorised"),
       ApiResponse(responseCode = "200", description = "OK")
     ]
   )
