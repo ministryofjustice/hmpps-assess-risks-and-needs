de6ba40711ea0bd918d2a850b34cdfd426796608
Natalie Wood <Natalie.wood@digital.justice.gov.uk>
2022-07-04 15:50:10 +0100

    update dependencies and remove broken id column from supplementary risk test data (#170)

--- src/test/resources/supplementaryrisk/before-test.sql
+++ src/test/resources/supplementaryrisk/before-test.sql
@@ -1,7 +1,6 @@
 -- noinspection SqlResolveForFile
 
-insert into supplementary_risk (supplementary_risk_id,
-                                supplementary_risk_uuid,
+insert into supplementary_risk (supplementary_risk_uuid,
                                 risk_source,
                                 source_id,
                                 crn,
@@ -11,8 +10,8 @@ insert into supplementary_risk (supplementary_risk_id,
                                 created_date,
                                 risk_answers)
 values
-(1, '2e020e78-a81c-407f-bc78-e5f284e237e5', 'INTERVENTION_REFERRAL', '182987872', 'X123458', 'risk for children','Gary cooper', 'DELIUS', '2019-11-14 09:00', null),
-(2, '4e020e78-a81c-407f-bc78-e5f284e237e5', 'INTERVENTION_REFERRAL', '3e020e78-a81c-407f-bc78-e5f284e237e5', 'X123457', 'risk to self', 'Gary C', 'DELIUS', '2019-11-14 09:05', null),
-(3, '5e020e78-a81c-407f-bc78-e5f284e237e5', 'INTERVENTION_REFERRAL', '7e020e78-a81c-407f-bc78-e5f284e237e5', 'X123456', 'risk to self', 'Gary C', 'DELIUS', '2019-11-14 09:07', null),
-(4, '6e020e78-a81c-407f-bc78-e5f284e237e5', 'INTERVENTION_REFERRAL', '7e020e78-a81c-407f-bc78-e5f284e237e9', 'X123456', 'risk to self', 'Gary C', 'DELIUS', '2019-11-14 09:06',
+('2e020e78-a81c-407f-bc78-e5f284e237e5', 'INTERVENTION_REFERRAL', '182987872', 'X123458', 'risk for children','Gary cooper', 'DELIUS', '2019-11-14 09:00', null),
+('4e020e78-a81c-407f-bc78-e5f284e237e5', 'INTERVENTION_REFERRAL', '3e020e78-a81c-407f-bc78-e5f284e237e5', 'X123457', 'risk to self', 'Gary C', 'DELIUS', '2019-11-14 09:05', null),
+('5e020e78-a81c-407f-bc78-e5f284e237e5', 'INTERVENTION_REFERRAL', '7e020e78-a81c-407f-bc78-e5f284e237e5', 'X123456', 'risk to self', 'Gary C', 'DELIUS', '2019-11-14 09:07', null),
+('6e020e78-a81c-407f-bc78-e5f284e237e5', 'INTERVENTION_REFERRAL', '7e020e78-a81c-407f-bc78-e5f284e237e9', 'X123456', 'risk to self', 'Gary C', 'DELIUS', '2019-11-14 09:06',
 '{"riskWho":"Risk to person","riskWhen":"When risk is greatest","riskNature":"Nature is risk","concernsSelfHarm":"Self harm concerns","concernsSuicide":"Suicide concerns","concernsHostel":"Hostel concerns","concernsVulnerability":"Vulnerability concerns"}');
