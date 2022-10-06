42fbdc7b909b05206a8842812671154cd52dc910
cairnsp <101661307+cairnsp@users.noreply.github.com>
2022-10-06 16:37:14 +0100

    ARN-000 fix Swagger rendering of LocalDateTime to remove milliseconds (#217)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/assessrisksandneeds/config/OpenApiConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/assessrisksandneeds/config/OpenApiConfiguration.kt
@@ -3,13 +3,23 @@ package uk.gov.justice.digital.hmpps.assessrisksandneeds.config
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.media.Schema
+import org.springdoc.core.SpringDocUtils
 import org.springframework.boot.info.BuildProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 @Configuration
 class OpenApiConfiguration(buildProperties: BuildProperties) {
   private val version: String = buildProperties.version
+
+  init {
+    val schema: Schema<LocalDateTime> = Schema<LocalDateTime>()
+    schema.example(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")))
+    SpringDocUtils.getConfig().replaceWithSchema(LocalDateTime::class.java, schema)
+  }
 
   @Bean
   fun customOpenAPI(buildProperties: BuildProperties): OpenAPI? = OpenAPI()
