2ef3f6d17fc72a5861678605f6ee7d38c2f4ec61
Lucas Cairns <lucas.cairns@gmail.com>
2022-10-07 10:57:49 +0100

    ARN-1119 - Create endpoint for the RoSH risk widget (#215)
    * Create endpoint for the RoSH risk widget
    
    * Rename fields for RoSH widget DTO
    
    * Refactor RoSH widget DTO
    
    * Remove additional error handling from the widget controller

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/assessrisksandneeds/api/model/RoshRiskWidgetDto.kt
@@ -0,0 +1,62 @@
+package uk.gov.justice.digital.hmpps.assessrisksandneeds.api.model
+
+import com.fasterxml.jackson.annotation.JsonView
+import io.swagger.v3.oas.annotations.media.Schema
+import uk.gov.justice.digital.hmpps.assessrisksandneeds.api.model.RiskLevel.HIGH
+import uk.gov.justice.digital.hmpps.assessrisksandneeds.api.model.RiskLevel.LOW
+import uk.gov.justice.digital.hmpps.assessrisksandneeds.api.model.RiskLevel.MEDIUM
+import uk.gov.justice.digital.hmpps.assessrisksandneeds.api.model.RiskLevel.VERY_HIGH
+import java.time.LocalDateTime
+
+data class RoshRiskWidgetDto(
+  @Schema(description = "Overall ROSH risk score", example = "VERY_HIGH")
+  @JsonView(View.CrsProvider::class, View.RiskView::class)
+  val overallRisk: String? = null,
+
+  @Schema(description = "Assessed on", example = "2021-10-10")
+  @JsonView(View.Probation::class, View.SingleRisksView::class)
+  val assessedOn: LocalDateTime? = null,
+
+  @Schema(
+    description = "Risk in the community",
+    example = "{" +
+      "  \"Public\": \"HIGH\"" +
+      "  \"Children\": \"LOW\"" +
+      "  \"Known Adult\": \"MEDIUM\"" +
+      "  \"Staff\": \"VERY_HIGH\"" +
+      "}"
+  )
+  @JsonView(View.CrsProvider::class, View.RiskView::class)
+  val riskInCommunity: Map<String, String?> = hashMapOf(),
+
+  @Schema(
+    description = "Risk in custody",
+    example = "{" +
+      "  \"Public\": \"HIGH\"" +
+      "  \"Children\": \"LOW\"" +
+      "  \"Known Adult\": \"MEDIUM\"" +
+      "  \"Staff\": \"VERY_HIGH\"" +
+      "  \"Prisoners\": \"MEDIUM\"" +
+      "}"
+  )
+  @JsonView(View.Probation::class, View.RiskView::class)
+  val riskInCustody: Map<String, String?> = hashMapOf(),
+) {
+  companion object {
+    fun riskLevelToString(riskLevel: RiskLevel?): String? {
+      return when (riskLevel) {
+        VERY_HIGH -> "VERY_HIGH"
+        HIGH -> "HIGH"
+        MEDIUM -> "MEDIUM"
+        LOW -> "LOW"
+        null -> null
+      }
+    }
+
+    fun mapRiskLevelsToStrings(risks: Map<String, RiskLevel?>): Map<String, String?> {
+      return risks.map {
+        it.key to riskLevelToString(it.value)
+      }.toMap()
+    }
+  }
+}
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/assessrisksandneeds/integration/WidgetControllerTest.kt
@@ -0,0 +1,69 @@
+package uk.gov.justice.digital.hmpps.assessrisksandneeds.integration
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.test.web.reactive.server.expectBody
+import uk.gov.justice.digital.hmpps.assessrisksandneeds.api.model.RoshRiskWidgetDto
+import java.time.LocalDateTime
+
+@AutoConfigureWebTestClient
+@DisplayName("Risk widget Tests")
+class WidgetControllerTest : IntegrationTestBase() {
+
+  private val crn = "X123456"
+
+  @Test
+  fun `get risk summary by crn for external provider`() {
+    webTestClient.get().uri("/risks/crn/$crn/widget")
+      .headers(setAuthorisation(roles = listOf("ROLE_CRS_PROVIDER")))
+      .exchange()
+      .expectStatus().isOk
+      .expectBody<RoshRiskWidgetDto>()
+      .consumeWith {
+        assertThat(it.responseBody).isEqualTo(
+          RoshRiskWidgetDto(
+            overallRisk = "VERY_HIGH",
+            assessedOn = null,
+            riskInCommunity = mapOf(
+              "Children" to "LOW",
+              "Public" to "MEDIUM",
+              "Known Adult" to "LOW",
+              "Staff" to "HIGH",
+            ),
+          )
+        )
+      }
+  }
+
+  @Test
+  fun `get risk summary by crn for probation practitioner`() {
+    webTestClient.get().uri("/risks/crn/$crn/widget")
+      .headers(setAuthorisation(roles = listOf("ROLE_PROBATION")))
+      .exchange()
+      .expectStatus().isOk
+      .expectBody<RoshRiskWidgetDto>()
+      .consumeWith {
+        assertThat(it.responseBody).isEqualTo(
+          RoshRiskWidgetDto(
+            overallRisk = "VERY_HIGH",
+            assessedOn = LocalDateTime.of(2021, 6, 21, 15, 55, 4),
+            riskInCommunity = mapOf(
+              "Children" to "LOW",
+              "Public" to "MEDIUM",
+              "Known Adult" to "LOW",
+              "Staff" to "HIGH",
+            ),
+            riskInCustody = mapOf(
+              "Children" to "LOW",
+              "Public" to "LOW",
+              "Known Adult" to "LOW",
+              "Staff" to "VERY_HIGH",
+              "Prisoners" to "HIGH",
+            ),
+          )
+        )
+      }
+  }
+}
