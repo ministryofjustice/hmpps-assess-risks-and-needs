7676dc7dd12a5cfe6fa6cf495fae1103fcb60e4a
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-10-18 12:08:35 +0100

    remove unused class

--- src/main/kotlin/uk/gov/justice/digital/hmpps/assessrisksandneeds/api/model/UserAccessResponse.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/assessrisksandneeds/api/model/UserAccessResponse.kt
@@ -1,12 +1,5 @@
 package uk.gov.justice.digital.hmpps.assessrisksandneeds.api.model
 
-data class UserAccessResponse(
-  val exclusionMessage: String?,
-  val restrictionMessage: String?,
-  val userExcluded: Boolean,
-  val userRestricted: Boolean,
-)
-
 data class CaseAccess(
   val crn: String,
   val userExcluded: Boolean,
