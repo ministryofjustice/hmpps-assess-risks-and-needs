a994caf2d2c3f60235f04ec0512a0e1226ed959e
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2023-11-07 10:21:40 +0000

    updated security config to not use deprecated features (#286)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/assessrisksandneeds/config/ResourceServerConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/assessrisksandneeds/config/ResourceServerConfiguration.kt
@@ -6,27 +6,29 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 
 @Configuration
 @EnableWebSecurity
 class ResourceServerConfiguration {
   @Bean
-  fun filterChain(http: HttpSecurity): SecurityFilterChain {
-    http.headers().frameOptions().sameOrigin().and()
-      .sessionManagement()
-      .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-      // Can't have CSRF protection as requires session
-      .and().csrf().disable()
-      .authorizeHttpRequests { auth ->
-        auth.requestMatchers(
-          "/webjars/**", "/favicon.ico", "/csrf",
-          "/health/**", "/info", "/ping",
-          "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html",
-        ).permitAll()
-          .anyRequest()
-          .authenticated()
-      }.oauth2ResourceServer().jwt().jwtAuthenticationConverter(AuthAwareTokenConverter())
-
-    return http.build()
-  }
+  fun filterChain(http: HttpSecurity): SecurityFilterChain =
+    http.sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+      .csrf { it.disable() }
+      .authorizeHttpRequests {
+        it.requestMatchers(
+          AntPathRequestMatcher("/webjars/**"),
+          AntPathRequestMatcher("/favicon.ico"),
+          AntPathRequestMatcher("/csrf"),
+          AntPathRequestMatcher("/health/**"),
+          AntPathRequestMatcher("/info/**"),
+          AntPathRequestMatcher("/v3/api-docs/**"),
+          AntPathRequestMatcher("/swagger-ui/**"),
+          AntPathRequestMatcher("/v3/api-docs.yaml"),
+          AntPathRequestMatcher("/swagger-ui.html"),
+          AntPathRequestMatcher("/actuator/**"),
+        ).permitAll().anyRequest().authenticated()
+      }.oauth2ResourceServer {
+        it.jwt { jwt -> jwt.jwtAuthenticationConverter(AuthAwareTokenConverter()) }
+      }.build()
 }
