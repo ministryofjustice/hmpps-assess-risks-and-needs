11152ef3ef511de3c163a45513a51bbc89af361e
devkokov <dm.kokov@gmail.com>
2025-10-13 14:05:22 +0100

    Add the ability to authenticate with the API from the Swagger Docs

--- src/main/kotlin/uk/gov/justice/digital/hmpps/assessrisksandneeds/config/OpenApiConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/assessrisksandneeds/config/OpenApiConfiguration.kt
@@ -1,9 +1,12 @@
 package uk.gov.justice.digital.hmpps.assessrisksandneeds.config
 
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.media.Schema
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springdoc.core.utils.SpringDocUtils
 import org.springframework.boot.info.BuildProperties
 import org.springframework.context.annotation.Bean
@@ -29,4 +32,16 @@ class OpenApiConfiguration(buildProperties: BuildProperties) {
       )
         .contact(Contact().name("HMPPS Digital Studio").email("feedback@digital.justice.gov.uk")),
     )
+    .components(
+      Components().addSecuritySchemes(
+        "bearer-jwt",
+        SecurityScheme()
+          .type(SecurityScheme.Type.HTTP)
+          .scheme("bearer")
+          .bearerFormat("JWT")
+          .`in`(SecurityScheme.In.HEADER)
+          .name("Authorization"),
+      ),
+    )
+    .addSecurityItem(SecurityRequirement().addList("bearer-jwt"))
 }
