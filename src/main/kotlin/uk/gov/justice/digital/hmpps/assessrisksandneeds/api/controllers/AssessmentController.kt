a307bc2f0919981cd1c356053894e5fee7ec7146
Alex Warhurst <174349012+alexw-moj@users.noreply.github.com>
2026-07-23 14:55:04 +0100

    Tidy

--- src/main/kotlin/uk/gov/justice/digital/hmpps/assessrisksandneeds/api/controllers/AssessmentController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/assessrisksandneeds/api/controllers/AssessmentController.kt
@@ -164,7 +164,7 @@ class AssessmentController(
       ApiResponse(responseCode = "404", description = "No complete assessments found or no section1 data available"),
     ],
   )
-  @PreAuthorize("hasAnyRole('ROLE_ARNS__EXTERNAL_API_RO', 'ROLE_CRS_PROVIDER')")
+  @PreAuthorize("hasRole('ROLE_ARNS__EXTERNAL_API_RO')")
   fun getMappasAssessmentData(
     @Parameter(description = "Identifier type (e.g. crn, pnc)", required = true, example = "crn")
     @PathVariable identifierType: String,
