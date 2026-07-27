9b0f3f362710c6c6f9325eb880e49ca4419e6336
alexw-moj <174349012+alexw-moj@users.noreply.github.com>
2026-07-27 14:43:34 +0100

    ARN-3183 Add data items to new integration endpoint (#436)
    * ARN-3183 Add endpoint for External API team

--- src/main/kotlin/uk/gov/justice/digital/hmpps/assessrisksandneeds/restclient/api/oasys/section/OasysSection1.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/assessrisksandneeds/restclient/api/oasys/section/OasysSection1.kt
@@ -6,9 +6,21 @@ import com.fasterxml.jackson.annotation.JsonProperty
 data class OasysSection1(
   @param:JsonProperty("everCommittedSexualOffence")
   val everCommittedSexualOffenceRaw: String?, // question 1.30
+
+  @param:JsonProperty("assessor")
+  val assessor: Assessor? = null,
+
+  @param:JsonProperty("countersigner")
+  val countersigner: Assessor? = null,
 ) {
   @get:JsonIgnore
   val everCommittedSexualOffence: Boolean? = everCommittedSexualOffenceRaw.yesNo()
+
+  @get:JsonIgnore
+  val assessorName: String? get() = assessor?.name
+
+  @get:JsonIgnore
+  val countersignerName: String? get() = countersigner?.name
 
   fun String?.yesNo() = when (this?.trim()?.lowercase()) {
     "yes" -> true
@@ -16,3 +28,8 @@ data class OasysSection1(
     else -> null
   }
 }
+
+data class Assessor(
+  @param:JsonProperty("name")
+  val name: String? = null,
+)
